a27b1ddf8e02e4d9a79539188f9f69682d802a77
sahil dahake <sahildahake1999@gmail.com>
2022-10-11 20:03:36 +0530

    commit

---
+++ 10.py
@@ -0,0 +1,18 @@
+# string functions
+mystr="this is my tenth python program"
+
+print(mystr.endswith("program"))
+print(mystr.endswith("sahil"))
+print(mystr.startswith("my"))
+print(mystr.startswith("this"))
+
+print(mystr.count("o"))
+print(mystr.count("i"))
+print(mystr.count("p"))
+
+print(mystr.capitalize())
+print(mystr.upper())
+print(mystr.lower())
+
+
+print(mystr.replace("my", "there"))
