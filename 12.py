c0b12e973b86689c5e185e01c265fdbfddf3e4e4
sahil dahake <sahildahake1999@gmail.com>
2022-10-12 10:55:22 +0530

    commit

---
+++ 12.py
@@ -0,0 +1,8 @@
+d1={"good":"bad", "angry":"happy", "head":"leg", "ram":"ravan", "krishna":"kansa","sahil":{"b":"morning","l":"noon","d":"night"}}
+print(d1["sahil"])
+
+d2=d1
+print(d2)
+
+del d2
+print(d1)
