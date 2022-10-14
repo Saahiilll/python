ea4ab294059e47d3f98d5e7287397a07778c842c
sahil dahake <sahildahake1999@gmail.com>
2022-10-14 11:07:03 +0530

    commit

---
+++ 19.py
@@ -0,0 +1,34 @@
+#operators
+
+a = 2
+b = 3
+
+#arithmatic operator
+print("the value of a+b is", a+b)
+print("the value of a-b is", a-b)
+print("the value of a*b is", a*b)
+print("the value of a/b is", a/b)
+
+
+#assignment operators
+c = 10
+c += 5
+c -= 5
+c *= 5
+print(c)
+
+
+#comparision operator
+d = (12<14)
+d = (12>=6)
+print(d)
+
+#logical operators
+
+bool1 = True 
+bool2 = False
+print("the value of bool1 and bool2 is",(bool1 and bool2))
+print("the value of bool1 or bool2 is",(bool1 or bool2))
+print("the value of not bool1 is", ( not bool1))
+print("the value of not bool2 is", ( not bool2))
+
