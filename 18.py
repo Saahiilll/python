180d117121655c92e54e247ea836f449c584b6cb
sahil dahake <sahildahake1999@gmail.com>
2022-10-14 10:38:04 +0530

    commit

---
+++ 18.py
@@ -0,0 +1,30 @@
+#data types#
+
+#integer
+a = 23
+
+#float
+b = 4.5
+
+#string
+c = "sahil"
+
+#boolean
+d = True
+
+#none
+e = None
+
+#printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+#printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
