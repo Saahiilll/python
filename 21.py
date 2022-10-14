f0c746c617d71a94a23885d1d7a95c1cb5aaee09
sahil dahake <sahildahake1999@gmail.com>
2022-10-14 11:52:53 +0530

    commit

---
+++ 21.py
@@ -0,0 +1,4 @@
+#write a program to add two numbers
+a = 2
+b = 3
+print("the sum of this two number is", a + b)
---
+++ 22.py
@@ -0,0 +1,5 @@
+#remainder program
+a = 4
+b = 2
+
+print("the remainder of this two number is :", a%b)
